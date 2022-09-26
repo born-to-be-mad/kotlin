070171c16de126294b8bea60a8c3f35447e577a8
Dzmitry Marudau <dzmitry_marudau@epam.com>
2022-09-26 23:33:45 +0300

    Functions 'all', 'none' and 'any' are interchangeable.

--- intro/src/main/kotlin/tasks/InterchangeablePredicates.kt
+++ intro/src/main/kotlin/tasks/InterchangeablePredicates.kt
@@ -7,29 +7,17 @@ package by.dma.tasks
  * @author dzmitry.marudau
  * @since 2022.09
  */
-fun List<Int>.allNonZero() = all {
-    it != 0
-}
+fun List<Int>.allNonZero() = all { it != 0 }
 
-fun List<Int>.allNonZero1() = none {
-    it == 0
-}
+fun List<Int>.allNonZero1() = none { it == 0 }
 
-fun List<Int>.allNonZero2() = !any {
-    it == 0
-}
+fun List<Int>.allNonZero2() = !any { it == 0 }
 
-fun List<Int>.containsZero() = any {
-    it == 0
-}
+fun List<Int>.containsZero() = any { it == 0 }
 
-fun List<Int>.containsZero1() = !all {
-    it != 0
-}
+fun List<Int>.containsZero1() = !all { it != 0 }
 
-fun List<Int>.containsZero2() = !none {
-    it == 0
-}
+fun List<Int>.containsZero2() = !none { it == 0 }
 
 fun main(args: Array<String>) {
     val list1 = listOf(1, 2, 3)
