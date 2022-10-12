d63c6ec63e6231a5552edf8fd41f848e26921e89
Dzmitry Marudau <dzmitry_marudau@epam.com>
2022-10-13 00:26:23 +0300

    Task - Game2048 and Game15: base implementations

--- intro/src/main/kotlin/games/board/BoardImpl.kt
+++ intro/src/main/kotlin/games/board/BoardImpl.kt
@@ -1,8 +1,83 @@
 package games.board
 
-import games.board.Direction.*
+fun createSquareBoard(width: Int): SquareBoard = SquareBoardImpl(width)
 
-fun createSquareBoard(width: Int): SquareBoard =TODO()
+fun <T> createGameBoard(width: Int): GameBoard<T> = GameBoardImpl(width)
 
-fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
+open class SquareBoardImpl(final override val width: Int) : SquareBoard {
+
+    var cells: Array<Array<Cell>> = arrayOf()
+
+    init {
+        cells += createArray(width)
+    }
+
+    private fun createArray(width: Int): Array<Array<Cell>> {
+        var cellsArray = arrayOf<Array<Cell>>()
+        for (i in 1..width) {
+            var array = arrayOf<Cell>()
+            for (j in 1..width) {
+                array += Cell(i, j)
+            }
+            cellsArray += array
+        }
+
+        return cellsArray
+    }
+
+    override fun getCellOrNull(i: Int, j: Int): Cell? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCell(i: Int, j: Int): Cell {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllCells(): Collection<Cell> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        TODO("Not yet implemented")
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class GameBoardImpl<T>(width: Int) : SquareBoardImpl(width), GameBoard<T> {
+
+    private val boardCellsMap: MutableMap<Cell, T?> = mutableMapOf()
+
+    override fun get(cell: Cell): T? {
+        TODO("Not yet implemented")
+    }
+
+    override fun all(predicate: (T?) -> Boolean): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun any(predicate: (T?) -> Boolean): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun find(predicate: (T?) -> Boolean): Cell? {
+        TODO("Not yet implemented")
+    }
+
+    override fun filter(predicate: (T?) -> Boolean): Collection<Cell> {
+        TODO("Not yet implemented")
+    }
+
+    override fun set(cell: Cell, value: T?) {
+        TODO("Not yet implemented")
+    }
+
+}
 
