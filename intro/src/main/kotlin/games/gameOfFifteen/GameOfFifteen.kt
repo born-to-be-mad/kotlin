b6fffce2c901ae97d231f9623781dd4374d5ce90
Dzmitry Marudau <dzmitry_marudau@epam.com>
2022-10-13 16:08:16 +0300

    Task - Game15: final step

--- intro/src/main/kotlin/games/gameOfFifteen/GameOfFifteen.kt
+++ intro/src/main/kotlin/games/gameOfFifteen/GameOfFifteen.kt
@@ -5,7 +5,7 @@ import games.board.GameBoard
 import games.board.createGameBoard
 import games.game.Game
 
-/*
+/**
  * Implement the Game of Fifteen (https://en.wikipedia.org/wiki/15_puzzle).
  * When you finish, you can play the game by executing 'PlayGameOfFifteen'.
  */
@@ -17,23 +17,44 @@ class GameOfFifteen(private val initializer: GameOfFifteenInitializer) : Game {
     private val board: GameBoard<Int?> = createGameBoard(4)
 
     override fun initialize() {
-        TODO("Not yet implemented")
+        board.getAllCells()
+            .zip(initializer.initialPermutation)
+            .forEach { (cell, value) ->
+                board[cell] = value
+            }
     }
 
     override fun canMove(): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun hasWon(): Boolean {
-        TODO("Not yet implemented")
+        var count = 1
+        val boardWidth = board.width
+        for (i in 1..boardWidth) {
+            for (j in 1..boardWidth) {
+                if (get(i, j) != count && count != boardWidth * boardWidth) {
+                    return false
+                }
+                count++
+            }
+        }
+        return true
     }
 
     override fun processMove(direction: Direction) {
-        TODO("Not yet implemented")
+        val emptyCell = board.getAllCells().first { board[it] == null }
+        val neighbour = board.run { emptyCell.getNeighbour(direction.reversed()) }
+        if (neighbour != null) {
+            board[emptyCell] = board[neighbour]
+            board[neighbour] = null
+        }
     }
 
     override fun get(i: Int, j: Int): Int? {
-        TODO("Not yet implemented")
+        board.run {
+            return get(getCell(i, j))
+        }
     }
 
 }
--- intro/src/test/kotlin/games/gameOfFifteen/TestGameOfFifteen.kt
+++ intro/src/test/kotlin/games/gameOfFifteen/TestGameOfFifteen.kt
@@ -8,14 +8,14 @@ import org.junit.Test
 
 class TestGameOfFifteen {
     private fun Game.asString() =
-            (1..4).joinToString("\n") { i ->
-                (1..4).joinToString(" ") { j ->
-                    get(i, j)?.let { "%2d".format(it) } ?: " -"
-                }
+        (1..4).joinToString("\n") { i ->
+            (1..4).joinToString(" ") { j ->
+                get(i, j)?.let { "%2d".format(it) } ?: " -"
             }
+        }
 
     class TestGameInitializer(
-            override val initialPermutation: List<Int>
+        override val initialPermutation: List<Int>
     ) : GameOfFifteenInitializer
 
     private fun testGame(initialPermutation: List<Int>, moves: List<Move>) {
@@ -28,62 +28,86 @@ class TestGameOfFifteen {
             Assert.assertTrue("The move for game of fifteen should be always possible", game.canMove())
             game.processMove(move.direction)
             val prev = moves[index - 1].board
-            Assert.assertEquals("Wrong result after pressing ${move.direction} " +
-                    "for\n$prev\n",
-                    move.board, game.asString())
+            Assert.assertEquals(
+                "Wrong result after pressing ${move.direction} " +
+                        "for\n$prev\n",
+                move.board, game.asString()
+            )
         }
     }
 
     data class Move(
-            val direction: Direction?,
-            val initialBoard: String
+        val direction: Direction?,
+        val initialBoard: String
     ) {
         val board: String = initialBoard.trimMargin()
     }
 
     @Test
     fun testMoves() {
-        testGame(listOf(3, 6, 13, 15, 7, 5, 8, 4, 14, 11, 12, 1, 10, 9, 2),
-                listOf(
-                        Move(null, """
+        testGame(
+            listOf(
+                3, 6, 13, 15,
+                7, 5, 8, 4,
+                14, 11, 12, 1,
+                10, 9, 2
+            ),
+            listOf(
+                Move(
+                    null, """
             | 3  6 13 15
             | 7  5  8  4
             |14 11 12  1
-            |10  9  2  -"""),
-                        Move(RIGHT, """
+            |10  9  2  -"""
+                ),
+                Move(
+                    RIGHT, """
             | 3  6 13 15
             | 7  5  8  4
             |14 11 12  1
-            |10  9  -  2"""),
-                        Move(DOWN, """
+            |10  9  -  2"""
+                ),
+                Move(
+                    DOWN, """
             | 3  6 13 15
             | 7  5  8  4
             |14 11  -  1
-            |10  9 12  2"""),
-                        Move(LEFT, """
+            |10  9 12  2"""
+                ),
+                Move(
+                    LEFT, """
             | 3  6 13 15
             | 7  5  8  4
             |14 11  1  -
-            |10  9 12  2"""),
-                        Move(UP, """
+            |10  9 12  2"""
+                ),
+                Move(
+                    UP, """
             | 3  6 13 15
             | 7  5  8  4
             |14 11  1  2
-            |10  9 12  -"""),
-                        Move(RIGHT, """
+            |10  9 12  -"""
+                ),
+                Move(
+                    RIGHT, """
             | 3  6 13 15
             | 7  5  8  4
             |14 11  1  2
-            |10  9  - 12""")
-                ))
+            |10  9  - 12"""
+                )
+            )
+        )
     }
 
     @Test
     fun testWinning() {
-        val game = newGameOfFifteen(TestGameInitializer(
-                (1..15).toList()))
+        val game = newGameOfFifteen(
+            TestGameInitializer((1..15).toList())
+        )
         game.initialize()
-        Assert.assertTrue("The player should win when the numbers are in order",
-                game.hasWon())
+        Assert.assertTrue(
+            "The player should win when the numbers are in order",
+            game.hasWon()
+        )
     }
 }
